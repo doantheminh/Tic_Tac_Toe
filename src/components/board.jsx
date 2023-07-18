142d0142064c6b20fbb75d0c4c18777a15294c71
doantheminh <doantheminh13@gmail.com>
2023-07-19 02:02:02 +0700

    update reset , player win and lost , uno(error)

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -4,16 +4,40 @@ import Square from "./square"
 const Board = () => {
     const [game, setGame] = useState([null, null, null, null, null, null, null, null, null])
     const [player, setPlayer] = useState(true)
-    const [timer, setTimer] = useState(3)
+    const [timer, setTimer] = useState(5)
+    const [isGamesEnd, setIsGamesEnd] = useState(false);
+    const [history, setHistory] = useState([])
 
     const handlePlay = (position) => {
-        const newGame = game.slice()
-        newGame[position] = player ? "X" : "O"
-        setGame(newGame)
-        setPlayer(!player)
-        setTimer(3)
-    }
+        if (!checkWinner() && !isGamesEnd) {
+            const newGame = game.slice()
+            newGame[position] = player ? "X" : "O"
+            setHistory([...history, newGame])
+            setGame(newGame)
+            setPlayer(!player)
+            setTimer(5)
 
+            if (checkWinner()) {
+                setIsGamesEnd(true);
+            }
+        }
+
+    }
+    const resetGame = () => {
+        setGame([null, null, null, null, null, null, null, null, null]);
+        setPlayer(true);
+        setTimer(5);
+        setIsGamesEnd(false);
+        setHistory([])
+    };
+    const goBack = () => {
+        if (history.length > 0) {
+            const prevGame = history[history.length - 1]
+            setGame([...prevGame])
+            setHistory(history.slice(0, -1))
+            setPlayer(!player)
+        }
+    }
     const handleAutoPlay = () => {
         const emptyGame = game.map((item, index) => item ? null : index).filter(item => item !== null)
         const position = emptyGame[Math.floor(Math.random() * emptyGame.length)]
@@ -23,7 +47,14 @@ const Board = () => {
     // handleAutoPlay()
 
     const listWinner = [
-        [0, 1, 2], [0, 4, 8], [0, 3, 6]
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
     ]
 
 
@@ -43,15 +74,27 @@ const Board = () => {
             handleAutoPlay()
         }
         const interval = setInterval(() => {
-            setTimer(timer - 1)
+            if (checkWinner()) {
+                setTimer(0)
+            } else {
+                setTimer(timer - 1)
+            }
+
         }, 1000)
+
 
         return () => clearInterval(interval)
     }, [timer, game, player])
 
     return <>
         <div>
-            <h2>Winner is: {checkWinner()}</h2>
+            {checkWinner() ? <div>
+                <h2>Winner is: {checkWinner()}</h2>
+                {checkWinner() ? <button onClick={resetGame}>reset</button> : ''}
+                <h2>Loser is: {checkWinner() === 'X' ? 'O' : checkWinner() === 'O' ? 'X' : ''}</h2>
+            </div> : <h2>Turn: {player ? 'X' : 'O'}</h2>}
+            <button onClick={goBack}>Go back</button>
+
             <h2>Timer: {timer}</h2>
         </div>
         <div className="grid grid-cols-3 gap-2">
